79d1e03faec5f5e93a9f4937cab7f1464561f544
josh-dimpas <jk.dimpas@academy.opswerks.com>
2026-04-17 07:57:07 +0800

    feat: (WIP) Game schema

---
+++ src/types/game.d.ts
@@ -0,0 +1,30 @@
+export interface Game {
+    id: number
+    players: [GamePlayer, GamePlayer]
+    config: GameConfig
+    state: GameState
+}
+
+export interface GamePlayer {
+    id: number
+    revealBuffer: Uint32Array
+}
+
+export interface GameConfig {
+    mapSeed: string
+    chunkSize: number
+    vectorOrientation: number
+    winPointsThreshold: number
+
+    sharedFog: boolean
+    timeLimit: number
+    mapSize: { width: number; height: number }
+    revealRadius: number
+}
+
+export interface GameState {
+    mapLayer: Uint32Array
+    dataLayer: Uint32Array // Contains map data
+
+    turn: GamePlayer['id']
+}
