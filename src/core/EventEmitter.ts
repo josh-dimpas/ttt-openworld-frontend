f50347d40bd4f4d643bec731045ced6b13045255
josh-dimpas <jk.dimpas@academy.opswerks.com>
2026-04-23 10:37:35 +0800

    feat: change events container as map instead

--- src/core/EventEmitter.ts
+++ src/core/EventEmitter.ts
@@ -1,27 +1,35 @@
 type EventCallback<T extends unknown[] = unknown[]> = (...args: T) => void;
 
 export class EventEmitter<T extends Record<string, unknown[]> = Record<string, unknown[]>> {
-    private _events: Map<string, EventCallback[]> = new Map();
+    private _events: Map<string, Map<number, EventCallback>> = new Map();
 
     // @ts-expect-error enforcing event callback with a generic passed on causes conflict on loose-typed event emitters
     on<K extends keyof T>(event: K, callback: EventCallback<T[K]>): void;
     on(event: string, callback: EventCallback): void;
-    on(event: keyof T | string, callback: EventCallback): void {
-        const callbacks = this._events.get(event as string) || [];
-        callbacks.push(callback);
+    on(event: keyof T | string, callback: EventCallback): number {
+        const callbacks = this._events.get(event as string) || new Map();
+
+        const id = Object.keys(callbacks).length;
+        callbacks.set(id, callback);
         this._events.set(event as string, callbacks);
+        return id;
     }
 
     // @ts-expect-error enforcing event callback with a generic passed on causes conflict on loose-typed event emitters
     off<K extends keyof T>(event: K, callback: EventCallback<T[K]>): void;
     off(event: string, callback: EventCallback): void;
-    off(event: keyof T | string, callback: EventCallback): void {
+    off(event: string, id: number): void;
+    off(event: keyof T | string, callback: number | EventCallback): void {
         const callbacks = this._events.get(event as string);
         if (!callbacks) return;
 
-        const index = callbacks.indexOf(callback);
-        if (index !== -1) {
-            callbacks.splice(index, 1);
+        const index =
+            typeof callback === "number"
+                ? callback
+                : [...callbacks.entries()].find(([_, v]) => v === callback)?.[0];
+
+        if (index !== undefined) {
+            callbacks.delete(index);
         }
     }
 
@@ -42,7 +50,7 @@ export class EventEmitter<T extends Record<string, unknown[]> = Record<string, u
         const callbacks = this._events.get(event as string);
         if (!callbacks) return;
 
-        for (const callback of callbacks) {
+        for (const callback of callbacks.values()) {
             callback(...args);
         }
     }
