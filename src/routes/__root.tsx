12cd5be110a0157d7ebb7e01525bab2aa3ed37a7
josh-dimpas <jk.dimpas@academy.opswerks.com>
2026-05-07 09:17:27 +0800

    feat: add home route

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,36 +10,26 @@ import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 
 import appCss from '../styles.css?url'
 
-import type { QueryClient } from '@tanstack/react-query'
 import { env } from '#/env'
+import type { UserData } from '#/types/auth'
+import type { QueryClient } from '@tanstack/react-query'
+import { TooltipProvider } from '@radix-ui/react-tooltip'
 
 interface MyRouterContext {
   queryClient: QueryClient
+  user?: UserData
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   head: () => ({
     meta: [
-      {
-        charSet: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        title: env.VITE_APP_NAME,
-      },
+      { charSet: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { title: env.VITE_APP_NAME },
     ],
-    links: [
-      {
-        rel: 'stylesheet',
-        href: appCss,
-      },
-    ],
+    links: [{ rel: 'stylesheet', href: appCss }],
   }),
   shellComponent: RootDocument,
-  // context: getContext,
 })
 
 function RootDocument({ children }: { children: React.ReactNode }) {
@@ -49,7 +39,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <HeadContent />
       </head>
       <body className="w-screen h-screen min-h-0">
-        {children}
+        <TooltipProvider>{children}</TooltipProvider>
         <TanStackDevtools
           config={{
             position: 'bottom-right',
---
+++ src/routes/index.tsx
@@ -0,0 +1,82 @@
+import { Header } from '#/components/Header'
+import { Logo } from '#/components/Logo'
+import { Button } from '#/components/ui/button'
+import { getCurrentUserFn } from '#/server/auth'
+import { getCurrentLobbyFn } from '#/server/lobbies'
+import { createFileRoute, Link } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: Home,
+  loader: async () => ({
+    session: await getCurrentUserFn(),
+    lobby: await getCurrentLobbyFn(),
+  }),
+})
+
+function Home() {
+  const { session, lobby } = Route.useLoaderData()
+
+  return (
+    <div className="flex flex-col h-screen">
+      <Header user={session} />
+      <div className="relative flex flex-col items-center gap-2 pt-[10vh] shrink grow">
+        <Logo />
+        <div className="max-h-[20vh] shrink grow" />
+
+        {/* Play Expansion */}
+        {session != null ? (
+          <div className="space-y-2 w-92.5">
+            {lobby && (
+              <Button
+                size="xl"
+                className="min-w-92.5"
+                variant="outline"
+                asChild
+              >
+                <Link to="/lobby">Open Lobby</Link>
+              </Button>
+            )}
+
+            {!lobby && (
+              <div className="flex flex-col">
+                <input type="checkbox" className="peer hidden" id="expand" />
+                <Button
+                  asChild
+                  size={'xl'}
+                  className="bg-destructive peer-checked:bg-primary peer-checked:shadow-none! min-w-92.5 peer-checked:translate-1! select-none"
+                >
+                  <label htmlFor="expand">Play</label>
+                </Button>
+
+                <div className="hidden peer-checked:flex lg:flex-row flex-col px-0 pl-1">
+                  <Button asChild variant={'secondary'}>
+                    <Link
+                      to="/create-game"
+                      search={{}}
+                      className="min-w-20 lg:min-w-45 text-xl uppercase tracking-wider btn grow"
+                    >
+                      Create
+                    </Link>
+                  </Button>
+                  <Button asChild variant={'accent'}>
+                    <Link
+                      to="/lobby/join"
+                      search={{}}
+                      className="min-w-20 lg:min-w-45 text-xl uppercase tracking-wider btn grow"
+                    >
+                      Join
+                    </Link>
+                  </Button>
+                </div>
+              </div>
+            )}
+
+            <Button className="bg-amber-500 w-full">History</Button>
+          </div>
+        ) : (
+          <div>- Please Login -</div>
+        )}
+      </div>
+    </div>
+  )
+}
